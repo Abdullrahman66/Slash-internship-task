da7f93dd7f6620104b40b26dfbe67bfd52dac139
abdelrahman hassan <abdelrahmanhassan20025@gmail.com>
2024-06-22 00:40:04 +0300

    Object detection using YOLO

--- Slash_Task.py
+++ Slash_Task.py
@@ -37,10 +37,8 @@ def detect_objects(image):
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
                 h = int(detection[3] * height)
-
                 x = int(center_x - w / 2)
                 y = int(center_y - h / 2)
-
                 boxes.append([x, y, w, h])
                 confidences.append(float(confidence))
                 class_ids.append(class_id)
