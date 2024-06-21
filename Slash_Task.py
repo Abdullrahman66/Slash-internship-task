d80158f29ae291211f08639ff82148fcfe9b825b
abdelrahman hassan <abdelrahmanhassan20025@gmail.com>
2024-06-22 00:15:10 +0300

    Object detection using YOLO

---
+++ Slash_Task.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+import streamlit as st
+from PIL import Image
+
+yolo = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+classes = []
+
+with open("coco.names", "r") as file:
+    classes = [line.strip() for line in file.readlines()]
+layer_names = yolo.getLayerNames()
+output_layers = [layer_names[i - 1] for i in yolo.getUnconnectedOutLayers()]
+
+
+# #Loading Images
+# name = "download.jpg"
+# img = cv2.imread(name)
+def detect_objects(image):
+    height, width, channels = image.shape
+
+    # # Detecting objects
+    blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+    yolo.setInput(blob)
+    outputs = yolo.forward(output_layers)
+
+    class_ids = []
+    confidences = []
+    boxes = []
+    for output in outputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    detected_classes = []
+    for i in range(len(boxes)):
+        if i in indexes:
+            # x, y, w, h = boxes[i]
+            # label = str(classes[class_ids[i]])
+            # cv2.rectangle(img, (x, y), (x + w, y + h), colorGreen, 3)
+            # cv2.putText(img, label, (x, y + 10), cv2.FONT_HERSHEY_PLAIN, 8, colorRed, 8)
+            label = str(classes[class_ids[i]])
+            detected_classes.append(label)
+    return detected_classes
+# print("Detected classes:", detected_classes)
+#cv2.imshow("Image", img)
+# cv2.imwrite("output.jpg",img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+# Streamlit UI
+st.title("YOLO Object Detection")
+st.write("Upload an image and click the 'Analyze' button to detect objects.")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.write("")
+    st.write("Analyzing...")
+
+    image_np = np.array(image)
+    detected_classes = detect_objects(image_np)
+
+    st.write("Detected classes:", detected_classes)
